e0b7e5051c8bc2fc5ce55edaa3f31bcc2d070307
endojun <38119840+enndou@users.noreply.github.com>
2018-05-30 15:25:00 +0900

    Create shell.sh

---
+++ shell.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+docker run -d -p 80:80 -v ~/docker/php70-apache/www:/var/www/html --name php70-apache php:7.0-apache
+git clone https://github.com/nagonagoya/Scheduler.git
+cp Scheduler/* ~/docker/php70-apache/www
+mkdir ~/docker/php70-apache/www/css
+mkdir ~/docker/php70-apache/www/favicon
+mkdir ~/docker/php70-apache/www/html
+mkdir ~/docker/php70-apache/www/js
+cp Scheduler/css/* ~/docker/php70-apache/www/css
+cp Scheduler/favicon/* ~/docker/php70-apache/www/favicon
+cp Scheduler/html/* ~/docker/php70-apache/www/html
+cp Scheduler/js/* ~/docker/php70-apache/www/js
