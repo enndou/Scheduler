d2c9799e960d7c91dc8d55d9ebc863112edd7f35
遠藤　諄 <38119840+enndou@users.noreply.github.com>
2018-06-04 11:51:50 +0900

    Update shell3.sh

--- shell3.sh
+++ shell3.sh
@@ -3,4 +3,4 @@
 #~/docker/nagoya/wwwにコンテンツを配置
 #ディレクトリがない場合は自動で作成する
 git clone https://github.com/nagonagoya/Scheduler.git ~/docker/nagoya/www
-docker run -d -p 80:80 -v ~/docker/nagoya/www/Scheduler:/var/www/html --name nagoya junendocreater/php
+docker run -d -p 80:80 -v ~/docker/nagoya/www:/var/www/html --name nagoya junendocreater/php
