fd3ebffb049fb3ec8d1ea6095b0da49f854037bb
遠藤　諄 <38119840+enndou@users.noreply.github.com>
2018-06-04 10:49:54 +0900

    Update shell2.sh

--- shell2.sh
+++ shell2.sh
@@ -2,7 +2,7 @@
 #Dockerを立ち上げてから実行する。
 #~/docker/php70-apache/wwwにコンテンツを配置
 
-docker run -d -p 80:80 -v ~/docker/php70-apache/www:/var/www/html --name php70-apache php:7.0-apache
+docker run -d -p 80:80 -v ~/docker/php70-apache/www:/var/www/html --name nagoya junendocreater/php:7.0-apache
 git clone https://github.com/nagonagoya/Scheduler.git
 cp Scheduler/* ~/docker/php70-apache/www
 mkdir ~/docker/php70-apache/www/css
